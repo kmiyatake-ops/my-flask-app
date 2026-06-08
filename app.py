95134943fe0a99f3c7fbee7c3b269f091992a0fb
宮武 佳祐 <kmiyatake@yamatomfg.local>
2026-06-08 14:33:58 +0900

    Replace qrcode with segno to avoid Pillow dependency

--- app.py
+++ app.py
@@ -81,8 +81,10 @@ def index():
     host = request.host
     url = f"http://{host}"
     
-    # QRコードを生成
+    # QRコードを生E
     qr = segno.make(url)
+    
+    # 画像をbase64エンコーチE
     buffered = BytesIO()
     qr.save(buffered, kind='png', scale=10)
     img_str = base64.b64encode(buffered.getvalue()).decode()
